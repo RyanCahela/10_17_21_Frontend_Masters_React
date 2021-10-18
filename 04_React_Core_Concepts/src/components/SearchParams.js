31a3f5d82463f92a7694c7c07cdb6364b675f19f
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-10-18 12:08:37 -0700

    added select boxes to search form

--- 04_React_Core_Concepts/src/components/SearchParams.js
+++ 04_React_Core_Concepts/src/components/SearchParams.js
@@ -2,9 +2,17 @@ import { useState } from "react";
 
 function SearchParams() {
   const [searchLocation, setSearchLocation] = useState("");
+  const [selectedAnimal, setSelectedAnimal] = useState("");
+  const [selectedBreed, setSelectedBreed] = useState("");
+
+  const ANIMAL = ["bird", "cat", "dog", "rabbit", "reptile"];
+  const BREED = ["test"];
   return (
     <div className="search-params">
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form
+        className="flex-col stack stack--small"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <label htmlFor="location">Location</label>
         <input
           type="text"
@@ -13,6 +21,34 @@ function SearchParams() {
           value={searchLocation}
           onChange={(e) => setSearchLocation(e.target.value)}
         />
+        <label htmlFor="animal">Animal</label>
+        <select
+          value={selectedAnimal}
+          onBlur={(e) => setSelectedAnimal(e.target.value)}
+          onChange={(e) => setSelectedAnimal(e.target.value)}
+        >
+          {ANIMAL.map((animal) => {
+            return (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            );
+          })}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          value={selectedBreed}
+          onBlur={(e) => setSelectedBreed(e.target.value)}
+          onChange={(e) => setSelectedBreed(e.target.value)}
+        >
+          {BREED.map((breed) => {
+            return (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            );
+          })}
+        </select>
         <button typw="submit">Submit</button>
       </form>
     </div>
