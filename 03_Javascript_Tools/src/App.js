073a610173ad53f39c8bfd3ca4b8e774895342a9
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-10-18 08:37:50 -0700

    added prettier

---
+++ 03_Javascript_Tools/src/App.js
@@ -0,0 +1,31 @@
+const Pet = ({ name, animalType, breed }) => {
+  return React.createElement("div", { className: "pet stack stack--small" }, [
+    React.createElement("h2", {}, name),
+    React.createElement("h3", {}, animalType),
+    React.createElement("h3", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { className: "stack stack--large" }, [
+    React.createElement("h1", {}, "Hello World!"),
+
+    React.createElement(Pet, {
+      name: "Luna",
+      animalType: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Chelsea",
+      animalType: "Dog",
+      breed: "Mix",
+    }),
+    React.createElement(Pet, {
+      name: "Timmy",
+      animalType: "Turtle",
+      breed: "Sea Turtle",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
